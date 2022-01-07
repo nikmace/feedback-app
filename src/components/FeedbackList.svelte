9ca048fc03f269080274f7bfe18a2d49d2e03e68
Nikita Baranov <nikmacentosh@gmail.com>
2022-01-07 13:57:56 +0200

    Svelte Components

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onDestroy } from "svelte";
+  import { FeedbackStore } from "../stores";
+  import { fade, scale } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
+
+
+  // let feedback = [];
+
+  // const unsubscribe = FeedbackStore.subscribe((data) => (feedback = data));
+
+  // onDestroy(() => {
+  //   unsubscribe();
+  // });
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <div in:scale out:fade>
+    <FeedbackItem item={fb} />
+  </div>
+{/each}
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { FeedbackStore } from "../stores";
+
+  $: count = $FeedbackStore.length;
+  $: average =
+    $FeedbackStore.reduce((a, { rating }) => a + rating, 0) /
+    $FeedbackStore.length;
+</script>
+
+<div class="feeback-stats">
+  <h4>{count} Reviews</h4>
+  <h4>Ratings Average: {average}</h4>
+</div>
+
+<style>
+  .feeback-stats {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
